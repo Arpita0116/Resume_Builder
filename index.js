bb5009f60086ed7ed7b5d636101306ce6dded953
Arpita0116 <166647545+ArpitaPatel15@users.noreply.github.com>
2024-07-22 15:21:48 +0530

    setup is done

---
+++ index.js
@@ -0,0 +1,34 @@
+let dbConnection = require('./config/db')
+let dotenv = require('dotenv')
+let express = require('express')
+const { authRoute } = require('./Route/authRoute')
+let app = express()
+let cors = require('cors')
+let PORT = process.env.PORT || 8000
+app.use(express.json())
+//middlware configration
+dotenv.config({})
+dbConnection()
+//for setting form data into req
+app.use(express.urlencoded({ extended: true }))
+//route
+//middlware for error handling
+app.use(cors())
+app.use('/auth/v1', authRoute)
+
+app.use((err, req, res, next) => {
+    if (err) {
+        res.status(err.status || 401);
+        res.send({
+            status: err.status || 401,
+            message: err.message,
+        });
+    }
+    else {
+        next()
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`backend is connected : http://127.0.0.1:${PORT}`)
+})
