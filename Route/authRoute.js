4bfceca78bd27f9e95ee99dc20c06aa32228f17a
Arpita0116 <166647545+ArpitaPatel15@users.noreply.github.com>
2024-07-22 15:28:48 +0530

    backend is done

---
+++ Route/authRoute.js
@@ -0,0 +1,19 @@
+let express = require('express')
+const { registrationController, loginController, verifyController, logoutController, logoutFromAllDeviceController, forgetPasswordController, refreshTokenController } = require('../Controller/authController')
+const { verifyToken } = require('../middlware/authMiddlware')
+let route = express.Router()
+//registration || POST
+route.post('/signup', registrationController)
+//login  || POST
+route.post('/signin', loginController)
+//logout || get
+route.get('/logout/:id', logoutController)
+//alllogout ||get
+route.get('/all-logout/:id', logoutFromAllDeviceController)
+//forget-password ||  post
+route.post('/forget-password', forgetPasswordController)
+//refresh-token || POST
+route.post('/refresh-token', refreshTokenController)
+//token || get
+route.get('/token', verifyToken, verifyController)
+module.exports = { authRoute: route }
---
+++ helper/authToken.js
@@ -0,0 +1,57 @@
+let jwt = require("jsonwebtoken");
+function accessTokenGenrator(userId) {
+    let secret = process.env.ACCESS_KEY;
+    return new Promise((res, rej) => {
+        let payload = {};
+        let option = {
+            expiresIn: "15sec",
+            issuer: "utsavmaithili@gmail.com",
+            audience: userId,
+        };
+        jwt.sign(payload, secret, option, (err, token) => {
+            if (err) {
+                rej(err.message);
+            }
+            res(token);
+        });
+    });
+}
+function refreshTokenGenrator(userId) {
+    let secret = process.env.REFRESH_KEY;
+    return new Promise((res, rej) => {
+        let payload = {};
+        let option = {
+            expiresIn: "1y",
+            issuer: "utsavmaithili@gmail.com",
+            audience: userId,
+        };
+        jwt.sign(payload, secret, option, (err, token) => {
+            if (err) {
+                rej(err.message);
+            }
+            res(token);
+        });
+    });
+}
+function refreshTokenVerify(token) {
+    try {
+        if (!token)
+            return res
+                .status(500)
+                .send({ message: "somthing wrong !", sucess: false });
+
+        let decode = jwt.verify(token, process.env.REFRESH_KEY);
+        if (!decode) res.status(401).send({ message: "unauthorized user" });
+        return decode;
+    } catch (err) {
+        if (err.name == "JsonWebTokenError") {
+            return res
+                .status(500)
+                .send({ message: "user is unauthorized", sucess: false });
+        } else {
+            return res.status(500).send({ message: err.message, sucess: false });
+        }
+    }
+}
+
+module.exports = { accessTokenGenrator, refreshTokenGenrator, refreshTokenVerify };
